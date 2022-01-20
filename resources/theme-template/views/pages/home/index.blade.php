e3083df92d14d4a521cdce8f2bda2d91d1289005
Egoist <epsilonelittr@gmail.com>
2022-01-21 00:52:47 +0300

    Update default theme

--- resources/theme-template/views/pages/home/index.blade.php
+++ resources/theme-template/views/pages/home/index.blade.php
@@ -1,21 +1,9 @@
 @extends('theme::layouts.main.layout')
 
 @section('content')
-    {{-- Hero --}}
-    @include('theme::components.common.hero')
+    @component('theme::components.common.breadcrumb')
 
-    @include('theme::components.landing.features')
-
-    @include('theme::components.landing.about')
-
-    @include('theme::components.landing.pricing')
-
-    @include('theme::components.landing.faq')
-
-    @include('theme::components.landing.testimonials')
-
-    @include('theme::components.landing.team')
-
-    @include('theme::components.landing.contact')
-
+    <h1>
+        Index Page
+    </h1>
 @endsection
